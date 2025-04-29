f170af6b2b1661a828b86b6abef41cc20716308b
sabrina220 <132724494+sabrina220@users.noreply.github.com>
2025-04-28 23:16:42 -0500

    Update Person.sql

--- Person.sql
+++ Person.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Person (
-	person_id INT PRIMARY KEY AUTO_INCREMENT,
-	first_name VARCHAR(255) NOT NULL,
-	last_name VARCHAR(255) NOT NULL
+    person_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
 );
