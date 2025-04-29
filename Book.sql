92d90186db4a820fa850bad8f1c68571f8959a69
sabrina220 <132724494+sabrina220@users.noreply.github.com>
2025-04-28 23:18:38 -0500

    Update Book.sql

--- Book.sql
+++ Book.sql
@@ -1,13 +1,10 @@
 CREATE TABLE Book (
-	book_id INT PRIMARY KEY AUTO_INCREMENT,
-	publish_year YEAR,
-	price DECIMAL(10,2),
-	title VARCHAR(255),
-	genre VARCHAR(100),
-	stock_quantity INT,
-	restock_threshold INT,
-	publisher_name VARCHAR(255) NOT NULL,
-	publisher_email VARCHAR(255) NOT NULL,
-	publisher_phone_number VARCHAR(50) NOT NULL,
-	FOREIGN KEY (publisher_id) REFERENCES Publisher(publisher_id)
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    publish_year YEAR,
+    price DECIMAL(10,2),
+    title VARCHAR(255),
+    genre VARCHAR(100),
+    publisher_name VARCHAR(255) NOT NULL,
+    publisher_email VARCHAR(255) NOT NULL,
+    publisher_phone_number VARCHAR(50) NOT NULL
 );
