cc6bc1296a24459aaf2055aaa22d8c8af572c7be
sabrina220 <132724494+sabrina220@users.noreply.github.com>
2025-04-28 23:20:21 -0500

    Update Book_Order.sql

--- Book_Order.sql
+++ Book_Order.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Book_Order (
-	order_id INT,
-	book_id INT,
-	quantity INT,
-	PRIMARY KEY (order_id, book_id),
-	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-	FOREIGN KEY (book_id) REFERENCES Book(book_id)
+    order_id INT,
+    book_id INT,
+    quantity INT,
+    PRIMARY KEY (order_id, book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Book(book_id)
 );
+
