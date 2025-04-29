e0a9493205b8d67e28534120f6b8611d840d24e9
sabrina220 <132724494+sabrina220@users.noreply.github.com>
2025-04-28 23:20:40 -0500

    Update Wrote.sql

--- Wrote.sql
+++ Wrote.sql
@@ -1,7 +1,10 @@
 CREATE TABLE Wrote (
-	book_id INT,
-	author_id INT,
-	PRIMARY KEY (book_id, author_id),
-	FOREIGN KEY (book_id) REFERENCES Book(book_id),
-	FOREIGN KEY (author_id) REFERENCES Author(author_id)
+    book_id INT,
+    author_id INT,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (author_id) REFERENCES Author(author_id)
 );
+
+
+
