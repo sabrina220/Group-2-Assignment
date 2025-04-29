1b4a52659ee66338288d2c51ef89d4a6d56093ee
sabrina220 <132724494+sabrina220@users.noreply.github.com>
2025-04-28 23:19:57 -0500

    Update Author.sql

--- Author.sql
+++ Author.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Author (
-	author_id INT PRIMARY KEY AUTO_INCREMENT,
-	person_id INT,
-	FOREIGN KEY (person_id) REFERENCES Person(person_id)
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    person_id INT,
+    FOREIGN KEY (person_id) REFERENCES Person(person_id)
 );
