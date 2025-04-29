a3d28ded18e945b24b0fcf15ee7ffe53561a0dfb
sabrina220 <132724494+sabrina220@users.noreply.github.com>
2025-04-28 23:17:56 -0500

    Update Employee.sql

--- Employee.sql
+++ Employee.sql
@@ -1,9 +1,11 @@
 CREATE TABLE Employee (
-	employee_id INT PRIMARY KEY AUTO_INCREMENT,
-	person_id INT NOT NULL,
-	position VARCHAR(255),
-	hire_date DATE,
-	manager_id INT,
-	FOREIGN KEY (person_id) REFERENCES Person(person_id),
-	FOREIGN KEY (manager_id) REFERENCES Employee(employee_id)
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    person_id INT NOT NULL,
+    current_customer_id INT,
+    position VARCHAR(255),
+    hire_date DATE,
+    manager_id INT,
+    FOREIGN KEY (person_id) REFERENCES Person(person_id),
+    FOREIGN KEY (current_customer_id) REFERENCES Customer(customer_id),
+    FOREIGN KEY (manager_id) REFERENCES Employee(employee_id)
 );
